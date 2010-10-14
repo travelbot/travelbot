7d60c25eb37a583040b5f42c3090f309e4025787
Ondrej Mirtes <ondrej@mirtes.cz>
2010-10-14 13:22:35 +0200

    Better trip template with duration and distance helpers.

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -18,14 +18,51 @@ abstract class BasePresenter extends Presenter
 		}
 	}
 	
+	/**
+	 * @return Nette\Context
+	 */
 	public function getContext()
 	{
 		return $this->getApplication()->getContext();
 	}
 	
+	/**
+	 * @return Doctrine\ORM\EntityManager
+	 */
 	public function getEntityManager()
 	{
 		return $this->getContext()->getService('Doctrine\ORM\EntityManager');
+	}
+	
+	/**
+	 * @return Nette\Templates\ITemplate
+	 */
+	protected function createTemplate()
+	{
+		$template = parent::createTemplate();
+		
+		// duration template helper
+		$template->registerHelper('duration', function($value) {
+			$seconds = $value;
+			$minutes = round($seconds / 60);
+			$hours = round($minutes / 60);
+			if ($minutes < 1) {
+				return $seconds . ' seconds';
+			}
+			if ($hours < 1) {
+				return $minutes . ' minutes';
+			}
+			return $hours . ' hours and ' . $minutes%60 . ' minutes';
+		});
+		
+		$template->registerHelper('distance', function($value) {
+			if (($value / 1000) > 1) {
+				return round($value / 1000) . ' kilometers';
+			}
+			return $value . ' meters';
+		});
+		
+		return $template;
 	}
 
 }
