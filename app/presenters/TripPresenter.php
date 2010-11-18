668b20128d63e2bf08aa2d1a0dbec9c036bde099
Ondrej Mirtes <ondrej@mirtes.cz>
2010-11-18 02:02:59 +0100

    New UI, lots of bugfixes and javascript refactoring completed.

--- app/presenters/TripPresenter.php
+++ app/presenters/TripPresenter.php
@@ -1,6 +1,5 @@
 <?php
 
-use Nette\Application\JsonResponse;
 use Nette\Environment;
 
 /**
@@ -9,45 +8,6 @@ use Nette\Environment;
  */
 class TripPresenter extends BasePresenter
 {
-
-    /**
-     * @author Petr Vales
-     * @version 4.11.2010
-     */
-    public function handlePois() {
-        $location = $this->request->post['location'];
-        $service = new LocationService;
-        $coords = $service->getCoordinates($location);
-        
-        $service = new PoiService($this->entityManager);
-
-        try {
-        	$key = Environment::getConfig('api')->gowallaKey;
-            $pois = $service->getPois($coords['latitude'], $coords['longitude'], new PoiGowallaMapper($key));
-        } catch (InvalidStateException $e) {
-            $this->terminate(new JsonResponse(array(
-				'status' => 'FAIL',
-            )));
-        }
-
-        $jsonResponse = array();
-        foreach ($pois as $poi) {
-            $jsonResponse['pois'][] = array(
-                'name' => $poi->name,
-                'types' => $poi->types,
-                'address' => $poi->address,
-                'latitude' => $poi->latitude,
-                'longitude' => $poi->longitude,
-                'url' => $poi->url,
-                'icon' => $poi->imageUrl,
-            );
-        }
-
-		$jsonResponse['status'] = 'OK';
-		$jsonResponse['latitude'] = $coords['latitude'];
-		$jsonResponse['longitude'] = $coords['longitude'];
-        $this->terminate(new JsonResponse($jsonResponse));
-    }
 
 	public function renderDefault()
 	{
@@ -62,7 +22,6 @@ class TripPresenter extends BasePresenter
 		$this->template->trip = $trip;
 
 		try {
-			// fallback in case of failed loading
 			$eventService = new EventService;
 			$config = Environment::getConfig('api');
 			$events = $eventService->getEvents(
@@ -80,14 +39,14 @@ class TripPresenter extends BasePresenter
 			$trip->arrival,
 			new ArticleWikipediaMapper()
 		);
-         try{
+		
+         try {
             $flightMapper = new FlightKayakMapper();
             $flightService = new FlightService($this->entityManager);
             $this->template->flights = $flightService->buildFlights($flightMapper, 'PRG','PAR',"12/29/2010","12/30/2010",'1','e','n');
          } catch(FlightException $e)  {
 
          }
-
 	}
 
 }
